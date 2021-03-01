4ecc3dc81693d5bb0aff4b067bb20240309e6d57
Andrii Petrovskyi <angpysha@gmail.com>
2021-03-01 14:37:12 +0200

    Initial popup navigation page

---
+++ Rg.Plugins.Popup/Platforms/Ios/Renderers/NavigationPopupPageRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Rg.Plugins.Popup.IOS.Renderers;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(NavigationPopupPage),typeof(NavigationPopupPageRenderer))]
+namespace Rg.Plugins.Popup.IOS.Renderers
+{
+    public class NavigationPopupPageRenderer : NavigationRenderer
+    {
+        
+    }
+}
